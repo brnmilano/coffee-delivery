443165bba83e09b4f23640e9a371a00d2ae612d2
Bruno <brnmilano.dev@gmail.com>
2024-06-20 17:32:14 -0300

    Criação do Componente Nossos Cafés

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,13 @@
 import Container from "../components/Container/Container";
 import Hero from "../components/Hero";
+import OurCoffees from "../components/OurCoffees";
 
 export default function Home() {
   return (
     <Container>
       <Hero />
+
+      <OurCoffees />
     </Container>
   );
 }
